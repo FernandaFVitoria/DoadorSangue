2953bdfd053f36ffa6355f0d9190252492076675
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-04 21:55:45 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int idade;
+
+Console.Write("Qual a sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine()!);
+bool podeSerDoador = (idade >= 18) && (idade <= 67);
+if (podeSerDoador)
+{ 
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine("Você pode ser doador de sangue.");
+}
+else
+{
+     Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você não pode ser doador de sangue.");
+
+}
